332e94d79e5a27dba2c4d58d79c428fd6472830b
NerfedWizard <loelnelson@gmail.com>
2021-12-15 23:02:33 -0600

    MySQLSetupProjectBacklog

--- PPMToolFullStack1/src/main/java/com/loel/domain/Project.java
+++ PPMToolFullStack1/src/main/java/com/loel/domain/Project.java
@@ -33,17 +33,17 @@ public class Project {
 	private Long id;
 	@NotBlank(message = "Project name is required")
 	private String projectName;
-	@NotBlank(message = "Project identifier is required")
+	@NotBlank(message = "Project Identifier is required")
 	@Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
 	@Column(updatable = false, unique = true)
 	private String projectIdentifier;
 	@NotBlank(message = "Project description is required")
 	private String description;
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private Date startDate;
-	@JsonFormat(pattern = "yyyy-MM-dd")
-	private Date endDate;
-	@JsonFormat(pattern = "yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date start_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private Date end_date;
+	@JsonFormat(pattern = "yyyy-mm-dd")
 	@Column(updatable = false)
 	private Date createdAt;
 	@JsonFormat(pattern = "yyyy-MM-dd")
@@ -61,10 +61,4 @@ public class Project {
 	protected void onUpdate() {
 		this.updatedAt = new Date();
 	}
-
-	public Object getProjectLeader() {
-
-		return null;
-	}
-
 }
