aa511f8529bac70a7fc1f3aa36c535372fef2a12
NerfedWizard <loelnelson@gmail.com>
2021-12-20 18:39:38 -0600

    Color Coding Tasks

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -8,6 +8,25 @@ class Backlog extends Component {
     const tasks = project_tasks_prop.map((project_task) => (
       <ProjectTask key={project_task.id} project_task={project_task} />
     ));
+    let todoItems = [];
+    let inProgressItems = [];
+    let doneItems = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(tasks[i]);
+
+      if (tasks[i].props.project_task.status === "TO_DO") {
+        todoItems.push(tasks[i]);
+      }
+
+      if (tasks[i].props.project_task.status === "IN_PROGRESS") {
+        inProgressItems.push(tasks[i]);
+      }
+
+      if (tasks[i].props.project_task.status === "DONE") {
+        doneItems.push(tasks[i]);
+      }
+    }
 
     return (
       <div className="container">
@@ -18,10 +37,7 @@ class Backlog extends Component {
                 <h3>TO DO</h3>
               </div>
             </div>
-            {tasks}
-            {
-              // insert tasks here
-            }
+            {todoItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -29,10 +45,7 @@ class Backlog extends Component {
                 <h3>In Progress</h3>
               </div>
             </div>
-            {
-              //  <!-- SAMPLE PROJECT TASK STARTS HERE -->
-              //         <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            {inProgressItems}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -40,10 +53,7 @@ class Backlog extends Component {
                 <h3>Done</h3>
               </div>
             </div>
-            {
-              // <!-- SAMPLE PROJECT TASK STARTS HERE -->
-              // <!-- SAMPLE PROJECT TASK ENDS HERE -->
-            }
+            {doneItems}
           </div>
         </div>
       </div>
