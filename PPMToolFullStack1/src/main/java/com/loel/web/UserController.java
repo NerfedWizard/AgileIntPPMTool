90b5425472ab5d028a506bd0211c9811e345f110
NerfedWizard <loelnelson@gmail.com>
2021-12-25 13:46:33 -0600

    JwtTokens

--- PPMToolFullStack1/src/main/java/com/loel/web/UserController.java
+++ PPMToolFullStack1/src/main/java/com/loel/web/UserController.java
@@ -5,6 +5,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +16,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.loel.domain.User;
+import com.loel.payload.JWTLoginSuccessResponse;
+import com.loel.payload.LoginRequest;
+import com.loel.security.JwtTokenProvider;
 import com.loel.services.MapValidationErrorService;
 import com.loel.services.UserService;
 import com.loel.validator.UserValidator;
+import static com.loel.security.SecurityConstraints.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -27,6 +35,27 @@ public class UserController {
 
 	@Autowired
 	private UserValidator userValidator;
+
+	@Autowired
+	private JwtTokenProvider tokenProvider;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@PostMapping("/login")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+		ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+		if (errorMap != null)
+			return errorMap;
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+		return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+	}
 
 	@PostMapping("/register")
 	public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
